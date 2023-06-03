9369f5773e99b3a06277efacc059a47cc4dbc7bc
Frank Shen <frank851207@gmail.com>
2023-06-03 14:32:41 -0700

    takeUntil init

--- takeUntil.js
+++ takeUntil.js
@@ -1,7 +1,22 @@
 const takeUntil = function(array, callback) {
   // ...
+  let result = [];
+  for(let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      break;
+    }
+    result.push(array[i]);
+  }
+  return result
 }
 
+// const fun = function(x) { 
+//   if(x === ","){
+//     return false;
+//   }
+//   return true;
+// }
+// console.log(fun(-1))
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
