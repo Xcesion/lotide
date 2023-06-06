9ccca865f910c5b564e56ad99bae850c532b5aec
Frank Shen <frank851207@gmail.com>
2023-06-06 01:51:09 -0700

    refactor middle.js

--- middle.js
+++ middle.js
@@ -1,23 +1,27 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length){
-    return false;
-  }
-  // let match = true;
-  for (let i = 0; i < arr1.length; i++){
-    if (arr1[i] !== arr2[i]){
-      return false;
-    }
-  }
-  return true;
-}
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length){
+//     return false;
+//   }
+//   // let match = true;
+//   for (let i = 0; i < arr1.length; i++){
+//     if (arr1[i] !== arr2[i]){
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)){
-    console.log(`✅✅✅ Assertion Passed: [ ${actual} ] === [ ${expected} ]`)
-  }else {
-    console.log(`🛑🛑🛑 Assertion Failed: [ ${actual} ] !== [ ${expected} ]`)
-  };
-};
+
+// const assertArraysEqual = function(actual, expected) {
+//   if (eqArrays(actual, expected)){
+//     console.log(`✅✅✅ Assertion Passed: [ ${actual} ] === [ ${expected} ]`)
+//   }else {
+//     console.log(`🛑🛑🛑 Assertion Failed: [ ${actual} ] !== [ ${expected} ]`)
+//   };
+// };
+
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = ("./assertArraysEqual")
 
 // ACTUAL FUNCTION
 const middle = function(array) {
@@ -35,9 +39,7 @@ const middle = function(array) {
       return arr;
     }
   }
-}
-// TEST CODE
-// ...
+};
 
-console.log(middle([1, 2, 3])) // => [2, 3]
-// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+module.exports = middle;
+
---
+++ test/middleTest.js
@@ -0,0 +1,6 @@
+const middle = require("../middle");
+
+// TEST CODE
+// ...
+console.log(middle([1, 2, 3])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
