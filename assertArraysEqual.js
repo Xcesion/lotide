e1d2fc5cb1711a4488ea518a6893a482beaf7684
Frank Shen <frank851207@gmail.com>
2023-06-06 01:39:10 -0700

    refector assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,17 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length){
-    return false;
-  }
-  // let match = true;
-  for (let i = 0; i < arr1.length; i++){
-    if (arr1[i] !== arr2[i]){
-      return false;
-    }
-  }
-  return true;
-}
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length){
+//     return false;
+//   }
+//   // let match = true;
+//   for (let i = 0; i < arr1.length; i++){
+//     if (arr1[i] !== arr2[i]){
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)){
@@ -19,5 +21,7 @@ const assertArraysEqual = function(actual, expected) {
   };
 };
 
-assertArraysEqual([1,2,3],[1,2,3])
-assertArraysEqual([1,2,3,4],[1,2,3,5])
+module.exports = assertArraysEqual;
+
+// assertArraysEqual([1,2,3],[1,2,3])
+// assertArraysEqual([1,2,3,4],[1,2,3,5])
