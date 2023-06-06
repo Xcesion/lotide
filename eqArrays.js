51930a0efddf3d537292e6d82fe1bf4478b12ede
Frank Shen <frank851207@gmail.com>
2023-06-06 01:24:05 -0700

    refector eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual , expected) {
-  if (actual === expected){
-    console.log(`✅✅✅ Assertion Passed: [ ${actual} ] === [ ${expected} ]`)
-  }else {
-    console.log(`🛑🛑🛑 Assertion Failed: [ ${actual} ] !== [ ${expected} ]`)
-  };
-  // return actual === expected;
-};
-
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length){
     return false;
@@ -22,6 +12,6 @@ const eqArrays = function(arr1, arr2) {
 
 }
 
+module.exports = eqArrays;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false) // => false
+
